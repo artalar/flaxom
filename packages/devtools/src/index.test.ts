7cf167e66099d9f0ad382d410f5e000ec28e6cad
Roman Agarkov <roman@reliant.ai>
2025-01-24 17:21:20 +0100

    chore: wip

--- packages/devtools/src/index.test.ts
+++ packages/devtools/src/index.test.ts
@@ -1,11 +1,9 @@
-import { describe, test, expect } from 'vitest'
+import { test, expect } from 'vitest'
 import { createTestCtx } from '@reatom/testing'
 
-describe('Stub Tests', () => {
-  test('stub', () => {
-    const ctx = createTestCtx()
+test('stub', () => {
+  const ctx = createTestCtx()
 
-    // expect.fail('No tests!')
-    expect(true).toBe(true) // Placeholder for an actual test
-  })
+  // expect.fail('No tests!')
+  expect(true).toBe(true) // Placeholder for an actual test
 })
